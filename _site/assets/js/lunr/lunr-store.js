f9eb030d9742743c4f5016d95b0d6bf8b305e860
Hyperidiot <tndvjd@gmail.com>
2021-01-23 00:43:26 +0900

    posting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "First Post",
-        "excerpt":"어제 한 5시간 내내 깃허브 블로그 구축을 위해 씨름을 했는데 드디어 성공했으므로 자축&amp;정상작동 확인용으로 첫 포스팅을 합니다.   Jekyll은 정말 설명 잘 해줬는데 내 컴퓨터가 m1칩이라 그런진 몰라도 에러를 계속 뿜길래 정말 좌절할 뻔 했다..   ","categories": [],
+        "excerpt":"어제 한 5시간 내내 깃허브 블로그 구축을 위해 씨름을 했는데 드디어 성공했으므로 자축&amp;정상작동 확인용으로 첫 포스팅을 한다.   Jekyll은 정말 설명 잘 해줬는데 내 컴퓨터가 m1칩이라 그런진 몰라도 에러를 계속 뿜길래 정말 좌절할 뻔 했다..   ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/first-article/",
         "teaser": null
@@ -15,5 +15,11 @@ var store = [{
         "excerpt":"변수 선언을 위해 사용하는 var는 사용을 피하는 게 좋다. var의 특징을 알아보자. var var는 초기 자바스크립트에서 등장했으며, let, const,와 상이한 점이 몇가지 존재한다. 구형 코드에서 var를 많이 볼 수 있는데, 이를 무턱대고 let으로 고치게 되면 코드가 엉망진창이 될 수 있다. 중복선언 가능 우선 가장 큰 특징으로는 중복선언이 가능하다는 것이다. var...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/var-let-const-diff/",
+        "teaser": null
+      },{
+        "title": "Pop Cat meme 애플리케이션",
+        "excerpt":"요즘 뜨고있는 pop cat을 컨트롤할 수 있는 애플리케이션을 만들어봤다. 그냥 귀여워서 만들었다. 코딩을 시작하면서 느낀 점이 있는데, 참 놀랍다. 만들고 싶다고 생각한 걸 눈에 보이는 아웃풋으로 변환할 수 있다니.. 단순히 컴퓨터와 키보드만 있다면 내 아이디어를 실현시킬 수 있는 것이다. 이번에는 이벤트 제어를 메인으로, 사용해본 적 없는 keydown 이벤트를 사용해봤다. 모바일...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/pop-cat/",
         "teaser": null
       }]
